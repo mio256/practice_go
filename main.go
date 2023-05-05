474b30609617a019f6609ce882602229f4d5eec4
mio256 <reiya@woorth.io>
2023-05-06 01:45:49 +0900

    :fire:

--- main.go
+++ main.go
@@ -1,55 +1,24 @@
 package main
 
-import "fmt"
-
-type Portion int
-
-const (
-	Regular Portion = iota
-	Small
-	Large
+import (
+	"fmt"
+	"strings"
+	"time"
 )
 
-type Udon struct {
-	men      Portion
-	aburaage bool
-	ebiten   uint
-}
-
-func NewUdon(p Portion, aburaage bool, ebiten uint) *Udon {
-	return &Udon{
-		men:      p,
-		aburaage: aburaage,
-		ebiten:   ebiten,
-	}
-}
-
-func NewKakeUdon(p Portion) *Udon {
-	return &Udon{
-		men:      p,
-		aburaage: false,
-		ebiten:   0,
-	}
-}
-
-func NewKitsuneUdon(p Portion) *Udon {
-	return &Udon{
-		men:      p,
-		aburaage: true,
-		ebiten:   0,
-	}
-}
-
-func NewTempuraUdon(p Portion) *Udon {
-	return &Udon{
-		men:      p,
-		aburaage: false,
-		ebiten:   3,
-	}
-}
-
 func main() {
-	t := NewUdon(Regular, false, 2)
+	s := time.Now()
 
-	fmt.Println(t)
+	var builder strings.Builder
+
+	builder.Grow(10000)
+
+	builder.WriteByte('a')
+
+	for i := 0; i < 10000; i++ {
+		builder.WriteByte('b')
+	}
+
+	fmt.Printf("%s\n", builder.String())
+	fmt.Printf("process time: %s\n", time.Since(s))
 }
